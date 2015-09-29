4fcaafc82becbc68c5f93280e8eee37e394149d4
Xiang Yu <eggplant.elf@gmail.com>
2015-09-29 14:51:34 +0200

    simple checker of code switch with dictionary

---
+++ check_dict.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+import tweepy
+import re
+import json
+from pymongo import MongoClient
+from segtok.segmenter import split_multi
+from segtok.tokenizer import word_tokenizer, split_contractions
+
+
+# filter @username, #topic and url
+filter_pattern = r'(@|#|https?:)\S*'
+
+
+def load_dict(de_file, tr_file):
+    de_words, tr_words = set(), set()
+    for line in open(de_file):
+        de_words.add(line.strip())
+    for line in open(tr_file):
+        tr_words.add(line.strip())
+    return de_words, tr_words
+
+
+
+def check(de_words, tr_words):
+    client = MongoClient()
+    tweets = client['twitter_tr']['tweets']
+
+    for tweet in tweets.find({'indexed': False}): #.limit(batch_size)
+        # tweets.update({'_id': tweet['_id']}, {'$set': {'indexed': True}})
+        text = re.sub(filter_pattern, '', tweet['text'])
+        total, de, tr = 0, 0, 0
+        de_list, tr_list = [], []
+        ans = False
+        for sent in split_multi(text):
+            for word in word_tokenizer(sent):
+                total += 1
+                if word in tr_words:
+                    tr += 1
+                    tr_list.append(word)
+                elif word in de_words: 
+                    de += 1
+                    de_list.append(word)
+                if tr >= 5 and de >= 2:
+                    ans = True
+        if ans:
+            print tweet['text']
+            print de_list
+
+
+
+if __name__ == '__main__':
+    de_words, tr_words = load_dict('dict_de.txt', 'dict_tr.txt')
+    check(de_words, tr_words)
