5973d62601529c0ecd2898690940ec32d66ee173
Xiang Yu <eggplant.elf@gmail.com>
2015-09-30 11:27:43 +0200

    add a field with value "false" into any collection in any db

--- add_field.py
+++ add_field.py
@@ -8,12 +8,15 @@ from pymongo import MongoClient
 
 
 
-def add_field(client, lang):
-    tweets = client['twitter_'+lang]['tweets']
-    tweets.update({}, {'$set': {'indexed': False}}, upsert = True, multi = True)
+def add_field(client, db, collection, key, value):
+    # tweets = client['twitter_'+lang]['tweets']
+    tweets = client[db][collection]
+    tweets.update({}, {'$set': {key: value}}, upsert = True, multi = True)
 
 
 if __name__ == '__main__':
     client = MongoClient()
-    for lang in ['en', 'de', 'tr']:
-        add_field(client, lang)
+    db = sys.argv[1]
+    collection = sys.argv[2]
+    key = sys.argv[3]
+    add_field(client, db, collection, key, False)
