23d3b037c75dafaf60f10158b4ad86c460192603
Xiang Yu <eggplant.elf@gmail.com>
2015-09-30 16:52:56 +0200

    read from db, check with morph and dictionary, write into db

--- check.py
+++ check.py
@@ -18,14 +18,6 @@ import subprocess
 filter_pattern = r'(@|#|https?:)\S*'
 
 
-# generator for reading tweets from db
-def tweet_stream(db):
-    client = MongoClient()
-    tweets = client[db]['tweets']
-    users = client[db]['users']
-#   for tweet in tweets.find({'user_id': '239308610'}):
-    for tweet in tweets.find():
-        yield tweet['text']
 
 def read_dict(dict_file, min_freq):
     d = set()
@@ -44,22 +36,32 @@ class Checker:
     it analyzes e.g. every 1000 tweets in one go, then reloads the analyzer
     """
 
-    def __init__(self, de_dict_file, tr_dict_file, min_freq = 5):
+    def __init__(self, source_db, target_db, de_dict_file, tr_dict_file, min_freq = 5):
+        self.client = MongoClient()
+        self.source_db = self.client[source_db]
+        self.target_db = self.client[target_db]
         self.de_dict = read_dict(de_dict_file, min_freq)
         self.tr_dict = read_dict(tr_dict_file, min_freq)
 
 
+    # generator for reading tweets from db
+    def tweet_stream(self):
+        for tweet in self.source_db['tweets'].find():
+            yield (tweet['text'], tweet['tweet_id'], tweet['user_id']) 
 
-    def check(self, stream):
+
+    def check(self):
         """
         analyzes every german word as well, if too slow, then change to analyze only non-turkish words
         """
         batch_num = 1
         size = 1000
-        for tweets in self.batch(stream, size):
+        # for each batch
+        for tweets in self.batch(self.tweet_stream(), size):
             print batch_num * size 
             batch_num += 1
             words, counts = self.tokenize(tweets)
+            wlist = words.strip().split()
             trs = self.morph_tr(words)
             des = self.morph_de(words)
             i = 0
@@ -67,21 +69,42 @@ class Checker:
             for count in counts:
                 tr = trs[i: i + count]
                 de = des[i: i + count]
+                ws = wlist[i: i + count]
                 i += count
                 is_switch = any((not t and d) for (t, d) in zip(tr, de)) and tr.count(True) >= tr.count(False)
                 ans.append((is_switch, tr, de))
 
-            for t,(a, tr, de) in zip(tweets, ans):
+            for (text, tid, uid), (a, tr, de) in zip(tweets, ans):
                 if a:
-                    print t.encode('utf-8')
-    #            print tr, de
-        # return ans
+                    de_list = [w for (w, d) in zip(ws, de) if d]
+                    self.log(text, tid, uid, de_list)
+
+
+    def log(self, text, tid, uid, de_list):
+        print tweet['text'].encode('utf-8')
+        print de_list
+        ################
+        # log the tweet
+        self.target_db['tweets'].insert({'tweet_id': tid,\
+                                   'user_id': uid,\
+                                   'text': text,\
+                                   'words': de_list})
+        # log the user
+        self.target_db['users'].update({'user_id': uid},\
+                                 {'$inc': {'count': 1}}, upsert = True)
+
+        # log the german words
+        for word in de_list:
+            self.target_db['words'].update({'word': word},\
+                                     {'$inc': {'count': 1}}, upsert = True)
+
+
 
     def batch(self, stream, size):
         out = []
         i = 0
-        for text in stream:
-            out.append(text)
+        for (text, tid, uid) in stream:
+            out.append((text, tid, uid))
             i += 1
             if i == size:
                 yield out
@@ -99,17 +122,17 @@ class Checker:
         counts = [] # [5, 12, 0, 3, ...] the counts of valid words for each tweet
         out = '' # one-word-per-line string of the tokenized words for morph analysis
         
-        for text in tweets:
+        for (text, tid, uid) in tweets:
             i = 0
             text = re.sub(filter_pattern, '', text)
             for sent in split_multi(text):
                 for token in word_tokenizer(sent):
-                    out += (token.encode('utf-8', 'ignore').title() + '\n')
+                    out += (token.encode('utf-8', 'ignore').title() + '\n') # Capitalized
                     i += 1
             counts.append(i)
         return out, counts
 
-
+    # does it case sensitive?
     def morph_tr(self, words):
         """
         morphological analysis for turkish words
@@ -120,9 +143,10 @@ class Checker:
         morphs = output.strip().split('\n\n')
         assert len(morphs) == len(words.strip().split('\n'))
         morph_ans = map(lambda x: x[-2:] != '+?', morphs)
-        dict_ans = [w in self.tr_dict for w in words.strip().split()]
+        dict_ans = [w.lower() in self.tr_dict for w in words.strip().split()]
         return [any(pair) for pair in zip(dict_ans, morph_ans)]
 
+    # already capitalized
     def morph_de(self, words):
         """
         morphological analysis for german words, exclude punctuation and numbers
@@ -133,10 +157,11 @@ class Checker:
         morphs = output.strip().split('\n\n')
         assert len(morphs) == len(words.strip().split('\n'))
         morph_ans = map(lambda x: x.split('\t')[2] not in ['_', '<+PUNCT>', '<+CARD>', '<+SYMBOL>'], morphs)
-        dict_ans = [w in self.de_dict for w in words.strip().split()]
+        dict_ans = [w.lower() in self.de_dict for w in words.strip().split()]
         return [any(pair) for pair in zip(dict_ans, morph_ans)]
 
 if __name__ == '__main__':
-    db = sys.argv[1]
-    checker = Checker('freq_de.txt', 'freq_tr.txt', 5)
-    checker.check(tweet_stream(db))
+    source_db = sys.argv[1]
+    target_db = 'new_switch'
+    checker = Checker(source_db, target_db, 'freq_de.txt', 'freq_tr.txt', 5)
+    checker.check()
