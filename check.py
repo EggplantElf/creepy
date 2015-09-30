3dc24bb056c42eb3e49bd463244b5feaea678cb2
Xiang Yu <eggplant.elf@gmail.com>
2015-09-30 16:14:39 +0200

    integrate morphological analysis and dictionary lookup

--- check.py
+++ check.py
@@ -5,6 +5,8 @@
 import tweepy
 import time
 import json
+import re
+import sys
 from pymongo import MongoClient
 from segtok.segmenter import split_multi
 from segtok.tokenizer import word_tokenizer, split_contractions
@@ -12,16 +14,29 @@ import pexpect
 import subprocess
 
 
+# filter @username, #topic and url
+filter_pattern = r'(@|#|https?:)\S*'
 
 
 # generator for reading tweets from db
-def tweet_stream():
+def tweet_stream(db):
     client = MongoClient()
-    tweets = client['twitter']['tweets']
-    users = client['twitter']['users']
+    tweets = client[db]['tweets']
+    users = client[db]['users']
 #   for tweet in tweets.find({'user_id': '239308610'}):
     for tweet in tweets.find():
         yield tweet['text']
+
+def read_dict(dict_file, min_freq):
+    d = set()
+    for line in open(dict_file):
+        tmp = line.strip().split()
+        word = tmp[0]
+        freq = int(tmp[1])
+        if freq >= min_freq:
+            d.add(word)
+    return d
+
 
 class Checker:
     """
@@ -29,15 +44,18 @@ class Checker:
     it analyzes e.g. every 1000 tweets in one go, then reloads the analyzer
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, de_dict_file, tr_dict_file, min_freq = 5):
+        self.de_dict = read_dict(de_dict_file, min_freq)
+        self.tr_dict = read_dict(tr_dict_file, min_freq)
+
+
 
     def check(self, stream):
         """
         analyzes every german word as well, if too slow, then change to analyze only non-turkish words
         """
         batch_num = 1
-        size = 10000
+        size = 1000
         for tweets in self.batch(stream, size):
             print batch_num * size 
             batch_num += 1
@@ -59,7 +77,7 @@ class Checker:
     #            print tr, de
         return ans
 
-    def batch(self, stream, size = 1000):
+    def batch(self, stream, size):
         out = []
         i = 0
         for text in stream:
@@ -80,18 +98,12 @@ class Checker:
 
         counts = [] # [5, 12, 0, 3, ...] the counts of valid words for each tweet
         out = '' # one-word-per-line string of the tokenized words for morph analysis
-        at = False
         
         for text in tweets:
             i = 0
+            text = re.sub(filter_pattern, '', text)
             for sent in split_multi(text):
                 for token in word_tokenizer(sent):
-                    if token == '@':
-                        at = True
-                        continue
-                    if at:
-                        at = False
-                        continue
                     out += (token.encode('utf-8', 'ignore').title() + '\n')
                     i += 1
             counts.append(i)
@@ -107,8 +119,9 @@ class Checker:
         output = lookup.communicate(input=words)[0]
         morphs = output.strip().split('\n\n')
         assert len(morphs) == len(words.strip().split('\n'))
-        return map(lambda x: x[-2:] != '+?', morphs)
-
+        morph_ans = map(lambda x: x[-2:] != '+?', morphs)
+        dict_ans = [w in self.tr_dict for w in words.strip().split()]
+        return [any(pair) for pair in zip(dict_ans, morph_ans)]
 
     def morph_de(self, words):
         """
@@ -119,9 +132,11 @@ class Checker:
         output = lookup.communicate(input=words)[0]
         morphs = output.strip().split('\n\n')
         assert len(morphs) == len(words.strip().split('\n'))
-        return map(lambda x: x.split('\t')[2] not in ['_', '<+PUNCT>', '<+CARD>', '<+SYMBOL>'], morphs)
-
+        morph_ans = map(lambda x: x.split('\t')[2] not in ['_', '<+PUNCT>', '<+CARD>', '<+SYMBOL>'], morphs)
+        dict_ans = [w in self.de_dict for w in words.strip().split()]
+        return [any(pair) for pair in zip(dict_ans, morph_ans)]
 
 if __name__ == '__main__':
-    checker = Checker()
-    checker.check(tweet_stream())
+    db = sys.argv[1]
+    checker = Checker('freq_de.txt', 'freq_tr.txt', 5)
+    checker.check(tweet_stream(db))
