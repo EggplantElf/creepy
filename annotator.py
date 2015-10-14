c5aa89f5866147341578e65235ac8a92b5b94eb9
Xiang Yu <eggplant.elf@gmail.com>
2015-10-14 16:40:05 +0200

    highlight

--- annotator.py
+++ annotator.py
@@ -9,21 +9,33 @@ import re
 import sys
 import readline
 from pymongo import MongoClient
-from termcolor import colored
+
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
 
 
 class Annotator:
     def __init__(self, db):
         self.tweets = MongoClient()[db]['tweets']
-        self.size = 10
 
     def start(self):
-        for tweet in self.tweets.find({'flag': 'x'}).limit(self.size):
+        for tweet in self.tweets.find({'flag': 'x'}):
             oid = tweet['_id']
-            print tweet['text'].encode('utf-8')
-            for word in tweet['words']:
-                print colored(word, 'red'),
-            print
+            text = tweet['text']
+            words = tweet['words']
+            self.show(text, words)
+            print color.CYAN + ', '.join(w.encode('utf-8') for w in words) + color.END
+
             flag = 'x'
             input_str = raw_input()
             # simple mark
@@ -36,7 +48,11 @@ class Annotator:
                 self.mark_all(tweet['words'], flag)
 
     # show highlighted text
-    # def show(self, text, words):
+    def show(self, text, words):
+        text = text.encode('utf-8')
+        for word in words:
+            text = re.sub(r'(%s)'%word, color.RED + r'\1' + color.END, text)
+        print text
 
 
     def mark(self, oid, flag):
@@ -48,9 +64,8 @@ class Annotator:
                             {'$set': {'flag': flag}}, multi = True)
 
 
-
-
 if __name__ == '__main__':
     db = sys.argv[1]
     annotator = Annotator(db)
     annotator.start()
+    # annotator.show(u'Über daß çok, ok', [u'daß', u'ok'])
