0f3f9fd171652e53420c5ed2f867d8dca9b5dd0a
Xiang Yu <eggplant.elf@gmail.com>
2015-11-11 15:34:37 +0100

    getting better vocabulary

---
+++ cross_check.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+def check(de_freq_file, en_freq_file, de_dict_file, ratio, min_de_freq, max_en_freq):
+    de_dict = {}
+    en_dict = {}
+    de_words = []
+    for line in open(de_freq_file):
+        items = line.strip().split()
+        w, f = items[0], int(items[1])
+        de_dict[w] = f
+        de_words.append(w)
+    for line in open(en_freq_file):
+        items = line.strip().split()
+        w, f = items[0], int(items[1])
+        en_dict[w] = f
+
+    out = open(de_dict_file, 'w')
+
+    for w in de_words:
+        de_freq = de_dict[w]
+        if de_freq >= min_de_freq:
+            if w not in en_dict:
+                out.write('%s\t%d\n' % (w, de_freq))
+            else:
+                en_freq = en_dict[w]
+                if (de_freq > en_freq * ratio and en_freq < max_en_freq):
+                    out.write('%s\t%d\n' % (w, de_freq))
+    out.close()
+
+
+if __name__ == '__main__':
+    de_freq_file = sys.argv[1]
+    en_freq_file = sys.argv[2]
+    de_dict_file = sys.argv[3]
+    ratio = 5
+    min_de_freq = 10
+    max_en_freq = 50
+
+    check(de_freq_file, en_freq_file, de_dict_file, ratio, min_de_freq, max_en_freq)
---
+++ morph_check.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import re
+import subprocess
+from itertools import izip
+
+bad_de = re.compile(r'_|<\+PUNCT>|<\+CARD>|<\+SYMBOL>|<\+NPROP>|<GUESSER>|<\^ABBR>')
+bad_tr = re.compile(r'\*UNKNOWN\*|\+Punct|\+Punc|\+Num')
+
+
+alphabet = re.compile(r'^[a-zA-ZßäöüÄÖÜçÇşŞğĞıİ\-]+$')
+
+
+def check_tr(freq_file, output_file, min_freq):
+    print 'reading words...'
+    d = {}
+    words = []
+    for line in open(freq_file):
+        items = line.strip().split()
+        w, f = items[0], int(items[1])
+        if f < min_freq:
+            break
+        d[w] = f
+        words.append(w)
+
+    print 'number of words with freq >= %d: %d' % (min_freq, len(words))
+
+    print 'checking morphology'
+    # title() or lower() or original?
+    input_str = '\n'.join(w.decode('utf-8').title().encode('utf-8') for w in words) + '\n'
+    # cmd = './bin/lookup -d -q -f bin/checker.script'
+    cmd = './bin/Morph-Pipeline/lookup -d -q -f bin/Morph-Pipeline/test-script.txt'
+    lookup = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    output = lookup.communicate(input=input_str)[0]
+    # print output
+    morphs = output.strip().split('\n\n')
+    assert len(words) == len(morphs)
+
+    out = open(output_file, 'w')
+
+    for (w, m) in izip(words, morphs):
+        if not bad_tr.search(m):
+            out.write('%s\t%d\n' % (w, d[w]))
+    out.close()
+
+
+def check_de(freq_file, output_file, min_freq):
+    print 'reading words...'
+    d = {}
+    words = []
+    for line in open(freq_file):
+        items = line.strip().split()
+        w, f = items[0], int(items[1])
+        if f < min_freq:
+            break
+        d[w] = f
+        words.append(w)
+
+    print 'number of words with freq >= %d: %d' % (min_freq, len(words))
+
+    print 'checking morphology'
+    # title() or lower() or original?
+    input_str = '\n'.join(w.decode('utf-8').title().encode('utf-8') for w in words) + '\n'
+    cmd = './bin/_run_smor.sh 2> /dev/null'
+    lookup = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    output = lookup.communicate(input=input_str)[0]
+    morphs = output.strip().split('\n\n')
+    assert len(words) == len(morphs)
+
+    out = open(output_file, 'w')
+
+    for (w, m) in izip(words, morphs):
+        if not bad_de.search(m):
+            out.write('%s\t%d\n' % (w, d[w]))
+    out.close()
+
+
+
+if __name__ == '__main__':
+    freq_file = sys.argv[1]
+    output_file = sys.argv[2]
+    # min_freq = int(sys.argv[3])
+    min_freq = 5
+    check_de(freq_file, output_file, min_freq)
+
+
+
+
+
